4812ed838e60a801b45c708bbb51dd5a6caa6125
Unknown <jalexpayan@gmail.com>
2017-08-18 22:49:52 -0700

    Actividad1
    Clase del 18 de Agosto

---
+++ Actividad1/app/src/main/java/mx/cetys/jorgepayan/actividad1/Main2Activity.java
@@ -0,0 +1,33 @@
+package mx.cetys.jorgepayan.actividad1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class Main2Activity extends AppCompatActivity {
+    TextView text_view_messageReceived;
+    Button button_goBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        text_view_messageReceived = (TextView) findViewById(R.id.text_view_showFromTextName);
+        button_goBack = (Button) findViewById(R.id.button_GoToMain);
+
+        Intent intent = getIntent();
+        String messageReceived = intent.getStringExtra(MainActivity.EXTRA_KEY);
+        text_view_messageReceived.setText(messageReceived);
+
+        button_goBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+    }
+}
